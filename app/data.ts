0981c9626da4b334f923f3c595af43ed670d6774
Kilua Zoldick <nangadoumer@gmail.com>
2025-03-25 16:22:52 +0100

    ajout des videos

--- app/data.ts
+++ app/data.ts
@@ -34,7 +34,7 @@ export const PROJECTS: Project[] = [
       'A modern, interactive todo list application with a clean design.',
     link: 'https://smart-todo-list-rho.vercel.app/',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+      'https://res.cloudinary.com/dfszxfj7n/video/upload/v1742915710/smarttodo_wzd9bx.mp4',
     id: 'project1',
   },
   {
@@ -42,7 +42,7 @@ export const PROJECTS: Project[] = [
     description: 'An E-commerce website with modern design.',
     link: 'https://gaston-shop.vercel.app/',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+      'https://res.cloudinary.com/dfszxfj7n/video/upload/v1742915718/gastonshop_ngrhdb.mp4',
     id: 'project2',
   },
 ]
